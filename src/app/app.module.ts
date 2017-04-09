1ca888a248af0d86690c1fd57bdc1bace83d6fe0
Akayeshmantha <akayeshmantha@outlook.com>
2017-04-09 22:59:34 +0530

    Initilizing the Rideon APP

---
+++ src/app/app.module.ts
@@ -0,0 +1,80 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { ItemDetailsPage } from '../pages/item-details/item-details';
+import { ListPage } from '../pages/list/list';
+import {Loader} from '../components/loader/loader';
+import {InitialPage} from '../pages/initial-page/initial'
+import {LoginPage} from '../pages/login/login';
+import {RegisterPage} from '../pages/Register/register';
+import {ForgotPage} from '../pages/forgotpassword/forgot';
+import {RegisterVehicle} from '../pages/registerVehicle/regsterVehicl';
+import {ItemCreatePage} from '../components/imagePicker/image';
+import {UserPage} from '../pages/user-profile/user';
+import {MyRidesPage} from '../pages/my-rides/myrides';
+import { HomePage } from '../pages/home/home';
+import {PickupComponent} from '../components/pickup/pickup';
+import {MapDirective} from '../components/map/map';
+import {AvailableCarsComponent} from '../components/available-cars/available-cars';
+import {PickupCarComponent} from '../components/pickup-car/pickup-car';
+import {AngularFireModule} from 'angularfire2';
+import {DestinationAddressComponent} from '../components/destination-address/destination-address';
+const firebaseConfig = {
+  apiKey: 'AIzaSyDA-ls4lOKcdks1v1wMSZABzEDsTx5AZkQ',
+  authDomain: 'authentication-4b637.firebaseapp.com',
+  databaseURL: 'https://authentication-4b637.firebaseio.com',
+  storageBucket: 'authentication-4b637.appspot.com',
+  messagingSenderId: '112566065962'
+};
+@NgModule({
+  declarations: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage,
+    Loader,
+    InitialPage,
+    LoginPage,
+    RegisterPage,
+    ForgotPage,
+    RegisterVehicle,
+    ItemCreatePage,
+    UserPage,
+    MyRidesPage,
+    HomePage,
+    PickupComponent,
+    PickupCarComponent,
+    MapDirective,
+    AvailableCarsComponent,
+    DestinationAddressComponent
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage,
+    Loader,
+    InitialPage,
+    LoginPage,
+    RegisterPage,
+    ForgotPage,
+    RegisterVehicle,
+    ItemCreatePage,
+    UserPage,
+    MyRidesPage,
+     HomePage,
+    PickupComponent,
+    PickupCarComponent,
+    MapDirective,
+    AvailableCarsComponent,
+    DestinationAddressComponent
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
